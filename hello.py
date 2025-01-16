025e6046d611568fa3be6c5b041e793070a6be9b
Pedrommano <160624863+Pedrommano@users.noreply.github.com>
2025-01-16 14:49:09 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -44,6 +44,7 @@ class User(db.Model):
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    role = SelectField(u'Role?:', choices=[('Administrator'), ('Moderator'), ('User')])
     submit = SubmitField('Submit')
 
 
@@ -66,11 +67,12 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     user_all = User.query.all();
+    role_all = Role.query.all();
     print(user_all);
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()                
+        user = User.query.filter_by(username=form.name.data).first()                        
         if user is None:
-            user_role = Role.query.filter_by(name='User').first();
+            user_role = Role.query.filter_by(name=form.role.data).first();
             user = User(username=form.name.data, role=user_role);
             db.session.add(user)
             db.session.commit()
@@ -81,4 +83,4 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False),
-                           user_all=user_all);
+                           user_all=user_all, role_all = role_all);
