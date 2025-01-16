e6bb9df09bae260cd336cba39a9b51d77320821d
Pedrommano <160624863+Pedrommano@users.noreply.github.com>
2025-01-16 19:31:19 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,35 +1,96 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Discipline(db.Model):
+    __tablename__ = 'disciplines'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    semester = db.Column(db.String(16))
+
+    def __repr__(self):
+        return f'<Discipline {self.name}>'
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    role = SelectField(u'Role?:', choices=[('Administrator'), ('Moderator'), ('User')])
+    submit = SubmitField('Submit')
+
+
+class DisciplineForm(FlaskForm):
+    name = StringField('Disciplina:', validators=[DataRequired()])
+    semester = SelectField('Semestre:', choices=[(str(i), f'{i}° semestre') for i in range(1, 7)])
+    submit = SubmitField('Cadastrar')
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role, Discipline=Discipline)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/disciplinas')
+
+@app.route('/disciplinas', methods=['GET', 'POST'])
 def disciplinas():
-    disciplinas = [
-        {'nome': 'DSWA5', 'semestre': '5° semestre'},
-        {'nome': 'PTBDSWS', 'semestre': '4° semestre'},
-        {'nome': 'GPSA5', 'semestre': '5° semestre'},
-    ]
-    return render_template('disciplinas.html', disciplinas=disciplinas)
-
-@app.route('/professores')
-def professores():
-    return render_template('blank.html')
-
-@app.route('/alunos')
-def alunos():
-    return render_template('blank.html')
-
-@app.route('/cursos')
-def cursos():
-    return render_template('blank.html')
-
-@app.route('/ocorrencias')
-def ocorrencias():
-    return render_template('blank.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    form = DisciplineForm()
+    disciplines = Discipline.query.all()
+    if form.validate_on_submit():
+        discipline = Discipline(name=form.name.data, semester=form.semester.data)
+        db.session.add(discipline)
+        db.session.commit()
+        return redirect(url_for('disciplinas'))
+    return render_template('disciplinas.html', form=form, disciplines=disciplines)
